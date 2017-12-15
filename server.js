fdc99ee1e84954d81aa93803a483d95c0c52f430
vit lenovo <gunnar.oledal@gmail.com>
2017-12-15 18:52:03 +0100

    adding heartbeat functionality

--- server.js
+++ server.js
@@ -13,13 +13,29 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
+function heartbeat() {
+    this.isAlive = true;
+}
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
   ws.on('message', (data) => {
       ws.send(data);
   });
 });
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping('', false, true);
+    });
+  }, 30000);
 
 // setInterval(() => {
 //   wss.clients.forEach((client) => {
